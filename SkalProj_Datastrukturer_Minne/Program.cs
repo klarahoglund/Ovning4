5cc6956acd7f35fb30fcfb9a7154546ca42d5e69
Klara Höglund <klarahoglund@hotmail.com>
2022-05-30 17:47:32 +0200

    Utan så mycket kommentarer

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace SkalProj_Datastrukturer_Minne
 {
@@ -8,8 +9,22 @@ namespace SkalProj_Datastrukturer_Minne
         /// The main method, vill handle the menues for the program
         /// </summary>
         /// <param name="args"></param>
+
+
+        static List<string> list = new List<string>();
+        static string newinput = "5";
+        static char firstChar;
+        static string firstCharAsString;
+        static string cleanString;
+        static string nextInLine;
+        static Queue<string> queue = new Queue<string>();
+        static Stack stackLine = new Stack();
+
+
+
         static void Main()
         {
+
 
             while (true)
             {
@@ -62,22 +77,85 @@ namespace SkalProj_Datastrukturer_Minne
         /// </summary>
         static void ExamineList()
         {
-            /*
-             * Loop this method untill the user inputs something to exit to main menue.
-             * Create a switch statement with cases '+' and '-'
-             * '+': Add the rest of the input to the list (The user could write +Adam and "Adam" would be added to the list)
-             * '-': Remove the rest of the input from the list (The user could write -Adam and "Adam" would be removed from the list)
-             * In both cases, look at the count and capacity of the list
-             * As a default case, tell them to use only + or -
-             * Below you can see some inspirational code to begin working.
-            */
 
-            //List<string> theList = new List<string>();
-            //string input = Console.ReadLine();
-            //char nav = input[0];
-            //string value = input.substring(1);
+            do
+            {
+                //Instruktioner
+                Console.WriteLine();
+                Console.WriteLine("****************************************");
+                Console.WriteLine("Please navigate through the menu by inputting (+, -,  0) of your choice"
+                    + "\n+. directly followed of what you want to add to the list. "
+                    + "\n-. directly followed of what you want to remove from the list."
+                    + "\n0. If you are done with this part."
+                    + "\np. print list.");
+                Console.WriteLine("****************************************");
+                Console.WriteLine();
 
-            //switch(nav){...}
+                newinput = Console.ReadLine();
+                try
+                {
+                    if (newinput != null)
+                    {
+                        firstChar = newinput[0]; // set input to the first char in an input line
+                        firstCharAsString = firstChar.ToString(); //Läser in hela strängen 
+                    }
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+
+                {
+                    //Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+
+                }
+
+                switch (firstCharAsString)
+                {
+                    case "0":
+                        list.Clear();
+                        break;
+
+                    case "+":
+                        cleanString = DeleteFirstLetter(newinput);
+                        //Console.WriteLine($"After: {cleanString}");
+                        list.Add(cleanString);
+                        //Console.WriteLine("Listans kapasitet är nu: " + list.Capacity);
+                        //Console.WriteLine("Listans antal är nu: " + list.Count);
+
+                        //
+                        break;
+                    case "-":
+                        cleanString = DeleteFirstLetter(newinput);
+                        list.Remove(cleanString);
+                        //Console.WriteLine("Listans kapasitet är nu: " + list.Capacity);
+                        //Console.WriteLine("Listans antal är nu: " + list.Count);
+
+                        break;
+                    case "p":
+                        Console.WriteLine();
+                        Console.WriteLine("**************************");
+                        Console.WriteLine("The list at this moment:");
+                        foreach (var item in list)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        Console.WriteLine("**************************");
+                        break;
+                    default:
+
+                        break;
+
+                }
+            } while (firstCharAsString != "0");
+
+        }
+
+        private static string DeleteFirstLetter(string newinput)
+
+        {
+            //  Console.WriteLine("Do it ");
+            newinput = newinput.Remove(0, 1);
+            //  Console.WriteLine($"In prog: {newinput}");
+            return newinput;
         }
 
         /// <summary>
@@ -85,18 +163,159 @@ namespace SkalProj_Datastrukturer_Minne
         /// </summary>
         static void ExamineQueue()
         {
-            /*
-             * Loop this method untill the user inputs something to exit to main menue.
-             * Create a switch with cases to enqueue items or dequeue items
-             * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
-            */
+
+            do
+            {
+                //Instruktioner
+                Console.WriteLine("Please navigate through the menu by inputting  (1, 2,  0) of your choice"
+                    + "\n*1 Open store and start que. "
+                    + "\n*2 Put in line. "
+                    + "\n*3 remove from line"
+                    + "\n*4 show line"
+                    + "\n*0 If you are done with this part.");
+                newinput = Console.ReadLine();
+                try
+                {
+                    if (newinput != null)
+                    {
+                        firstChar = newinput[0]; // set input to the first char in an input line
+                        firstCharAsString = firstChar.ToString(); //Läser in hela strängen 
+                    }
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+
+                {
+                    //Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+
+                }
+
+                switch (firstCharAsString)
+                {
+                    case "0":
+
+                        break;
+                    case "1":
+                        OpenStore();
+                        break;
+                    case "2":
+                        PutInLine();
+                        break;
+                    case "3":
+                        DoneInLine();
+                        break;
+                    case "4":
+                        MyQue(queue);
+                        break;
+                    default:
+
+                        break;
+
+                }
+            } while (firstCharAsString != "0");
+
+
+
+            {
+                /*
+                 * Loop this method untill the user inputs something to exit to main menue.
+                 * Create a switch with cases to enqueue items or dequeue items
+                 * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
+                */
+            }
+        }
+
+        private static void MyQue(Queue<string> queue)
+        {
+            foreach (var person in queue)
+            {
+                Console.WriteLine(person + ", ");
+            }
+        }
+
+
+
+
+        private static void DoneInLine()
+        {
+            queue.Dequeue();
+
+        }
+
+        private static void PutInLine()
+        {
+            Console.WriteLine("Enter the name of the person who is joining the que: ");
+            nextInLine = Console.ReadLine();
+            queue.Enqueue(nextInLine);
+        }
+
+        private static void OpenStore()
+        {
+            Console.WriteLine("I am open");
         }
 
         /// <summary>
         /// Examines the datastructure Stack
         /// </summary>
         static void ExamineStack()
+
         {
+            do
+            {
+                //Instruktioner
+                Console.WriteLine("STACK: Please navigate through the menu by inputting  (1, 2, 3, 4,  0) of your choice"
+                    + "\n1. Open store and start que. "
+                    + "\n2. Put in line. "
+                    + "\n3. remove from line"
+                    + "\n4. show line"
+                    + "\n5. Reverse text"
+                    + "\n0. If you are done with this part.");
+                newinput = Console.ReadLine();
+                try
+                {
+                    if (newinput != null)
+                    {
+                        firstChar = newinput[0]; // set input to the first char in an input line
+                        firstCharAsString = firstChar.ToString(); //Läser in hela strängen 
+                    }
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+
+                {
+                    //Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+
+                }
+
+                switch (firstCharAsString)
+                {
+                    case "0":
+                        list.Clear();
+                        break;
+                    case "1":
+                        OpenStore();
+                        break;
+                    case "2":
+                        PutInStackLine();
+                        break;
+                    case "3":
+                        DoneInStackLine();
+                        break;
+                    case "4":
+                        MyStackLine();
+                        break;
+                    case "5":
+                        ReverseText();
+                        break;
+                    default:
+
+                        break;
+
+                }
+            } while (firstCharAsString != "0");
+
+
+
             /*
              * Loop this method until the user inputs something to exit to main menue.
              * Create a switch with cases to push or pop items
@@ -104,8 +323,135 @@ namespace SkalProj_Datastrukturer_Minne
             */
         }
 
+        private static void ReverseText()
+        {
+            string text;
+
+            Console.WriteLine("Enter some text to reverse: ");
+            Stack reverser = new Stack();
+
+            text = Console.ReadLine();
+            char[] chars = text.ToCharArray();
+
+            foreach (var item in chars)
+            {
+                reverser.Push(item);
+
+            }
+            foreach (var item in reverser)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+
+        private static void MyStackLine()
+        {
+            Console.WriteLine("This is th line: ");
+            foreach (var item in stackLine)
+            {
+                Console.WriteLine(item + ", ");
+            }
+
+        }
+
+        private static void DoneInStackLine()
+        {
+            //vänd stacken 
+            Stack temp = new Stack();
+
+            foreach (var item in stackLine)
+            {
+                temp.Push(item);
+            }
+
+            temp.Pop();
+            stackLine.Clear();
+            foreach (var item in temp)
+            {
+                stackLine.Push(item);
+            }
+        }
+
+        private static void PutInStackLine()
+        {
+            Console.WriteLine("Enter the name of the person who is joining the que: ");
+            nextInLine = Console.ReadLine();
+            stackLine.Push(nextInLine);
+        }
+
         static void CheckParanthesis()
         {
+            List<char> opens = new List<char>();
+            List<char> listA = new List<char>();
+            List<char> listB = new List<char>();
+            bool okay = true;
+
+            Console.WriteLine("Enter a string for examination. ");
+
+            string text = Console.ReadLine();
+            char[] theString = text.ToCharArray();
+
+
+            //Check att första är en öppningparantes
+
+            for (int i = 0; i < theString.Length; i++)
+            {
+                if (theString[i] == '(' || theString[i] == ')')
+                {
+                    opens.Add(theString[i]);
+                }
+            }
+
+            if (opens[0] != '(')
+            {
+                okay = false;
+            }
+                
+            
+
+
+            for (int i = 0; i < theString.Length; i++)
+            {
+                if (theString[i] == '(')
+                {
+                    listA.Add(theString[i]);
+
+                }
+                else if (theString[i] == ')')
+                {
+                    listB.Add(theString[i]);
+                }
+            }   
+            if (listA.Count != listB.Count)
+            {
+                okay = false;
+            }
+
+
+            for (int i = 0; i < listA.Count; i++)
+            {
+                if (listA[i] != '(' && listB[i] != ')') { okay = false; break; }
+            }
+
+
+            if (okay)
+            {
+                Console.WriteLine("Det funkar fint med paranteserna");
+            }
+            else if (!okay)
+            {
+                Console.WriteLine("Det är fel med paranterserna eller det finns inga");
+
+            }
+
+
+
+
+
+
+
+
             /*
              * Use this method to check if the paranthesis in a string is Correct or incorrect.
              * Example of correct: (()), {}, [({})],  List<int> list = new List<int>() { 1, 2, 3, 4 };
@@ -114,6 +460,7 @@ namespace SkalProj_Datastrukturer_Minne
 
         }
 
+        }
     }
-}
+
 
---
+++ SkalProj_Datastrukturer_Minne/QandA.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkalProj_Datastrukturer_Minne
+{
+    internal class QandA
+    {
+
+
+        /* ***** Q1: Hur fungerar stacken och heapen? *****
+        STACKEN 
+        skulle kunna liknas vid ett arbetsminne: här hålls alla trådar som utförs (processerna för metoder som utförs och alla anrop)
+        Det är som ett tillfälligt minne, ofta beskrivet som lådor staplade på varandra där bara den översta lådan är åtkomlig. När en "låda" är avklarad 
+        så hivas den och nästa låda blir åtkomlig. STACKEN städas så att säga direkt och ingenting sparas här. Här sparas också alla POINTERS eller adresser 
+        till REFERENSTYPER, alltså behöver vi "gå igenom" STACKEN för att komma åt våra REFERENSTYPER (som ALLTID finns i HEAPEN). 
+
+        HEAPEN
+        är ett långtidsminne där "saker" endast försvinner när städnig genomförs, alltså när GARBAGE COLLECTORN sätter igång. Här sparas "saker" som vi 
+        kan komma åt "direkt". Datan/Informationen/Sakerna är direktåtkomliga, som tvätt utspridd på en säng eller där "saker" står på rad i en hylla, 
+        är synliga och bara att plocka ner -utan någon speciell ordning.
+
+        **********************************************************************************************************************************************************
+
+         ***** Q2: Vad är Value Types repsektive Reference Types och vad skiljer dem åt? *****
+        REFERENCETYPES
+        sparas alltid på HEAPEN och har alltid en POINTER till den på STACKEN, vi måste alltså gå igenom STACKEN för att komma åt den. När vi hanterar REFERENCETYPES
+        hanterar alltså inte själva "saken" utan POINTERN på STACKEN
+        Exempel på REFERENCETYPES: Klass
+        (Ärver av System.Object)
+
+        VALUETYPES (och POINTERS)
+        Finns alltid där de är skapade, antingen på STACKEN eller på HEAPEN. När vi handterar VALUETYPES använder vi oss av "saken" direkt, som hårddata.
+        När VALUETYPES deklareras i en metod (som processas på STACKEN) så skapas den på STACKEN.
+        När en VALUETYPE deklareras utanför en metod, men i en REFERENSETYPE (Klass tex) så skapas den inne i REFERENSTYPEN på HEAPEN.
+        Exempel på VALUETYPES: Simple Types, Struct Types
+        (Ärver av System.ValueTypes)
+
+        **********************************************************************************************************************************************************
+        
+          ***** Q3: Metodernas olika utfall. *****
+          *********** A: Metoden som returnerar 3 ***********
+          
+        
+        public int ReturnValue()  => Metod vilket gör att Minnesutrymmet som används under processen är STACKEN
+            {  
+                 int x = new int(); => int är objekt av typen VALUETYPE (döpt till x). Då den skapas på STACKEN sparas den på STACKEN
+                 x = 3;  => Eftersom x är en VALUETYPE betyder = att vi sätter innehållet i objeketet till 3 "Hårdkodat på själva objektet"
+                 int y = new int();  => Här skapas ännu ett objekt (döpt till y) av VALUETYPE på STACKEN
+                 y = x; => Här vi sätter innehållet i objekt(y) till samma som objekt(x) är. Alltså två objekt som båda innehåller 3. Vi har alltså två stycken 3:or      
+                 y = 4; => Här sätter vi innehållet i objek(y) till 4.           
+                 return x;  => Vi har inte gjort om objekt(x), utan har ett objekt(y) som är 4 och ett objekt(x) som är 3. Returen blir alltså 3.
+            }  
+          
+
+           *********** B: Metoden som returnerar 4 ***********
+
+
+            public int ReturnValue2()   => Metod vilket gör att Minnesutrymmet som används är STACKEN
+            {  
+                  MyInt x = new MyInt();  => Här finns det en klass i bakgrunden (MyInt)vilket gör objektet till en REFERENSTYPE. Objektet, objekt(x), sparas på HEAPEN, med POINTERN referens(x) kopplad till den på STACKEN
+                  x.MyValue = 3;       => Genom att använda POINTERN ref(x) till objekt(x) på HEAPEN sätter vi innehållet för objekt(x) till 3
+                  MyInt y = new MyInt();  => Här skapas ännu ett objekt(y) som sparas på HEAPEN med POINTERN ref(y) kopplad till sig på STACKEN
+                  y = x;                 => Här sätts POINTERN ref(y) att peka till objekt(x). De två referenserna pekar nu till samma objekt(x). Inget MyValue är satt på y. 
+                  y.MyValue = 4;       =>  Genom att använda POINTERN ref(y) som också nu pekar mot objekt(x) sätts innehållet till 4          
+                  return x.MyValue;     => Ref(x) pekar mor objekt(x) som nu är 4
+             }
+           *
+          *  
+          *   **********************************************************************************************************************************************************
+          *   Övn 1.2: När ökar listans kapacitet? (Alltså den underliggande arrayens storlek)
+          *         När 4 element lags till
+          *   1.3 Med hur mycket ökar kapaciteten?
+          *         Den dubblerar sig 
+          *   1.4  
+          *   1.5 Nej, listan minskas inte
+          *   1.6 När man behöver spara minne
+          *   
+          *   
+          *   
+         * */
+    }
+}
